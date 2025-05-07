adac064e6c6a6bae575c1be9816cfc7a97f5201f
mdih16 <91021533+mdih16@users.noreply.github.com>
2025-05-07 15:58:47 +0200

    removed white background from spinner component

--- encrona-dash/src/components/spinner.tsx
+++ encrona-dash/src/components/spinner.tsx
@@ -1,6 +1,6 @@
 export default function Spinner() {
   return (
-    <div className="flex items-center justify-center bg-white">
+    <div className="flex items-center justify-center">
       <div
         className="h-14 w-14 animate-spin rounded-full border-4 border-solid"
         style={{
